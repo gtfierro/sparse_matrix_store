c149ff04ab585bacfd27c7765ad6b5457d88910c
Gabe Fierro <gtfierro@mines.edu>
2025-04-09 18:29:04 -0600

    cleaning up

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,9 @@
 import time
 from rdflib import Graph, URIRef
-from store import SparseMatrixGraph
+
+# from store import SparseMatrixG
+import store
+
 
 def benchmark_graph_loading(graph_type, graph_name):
     """
@@ -20,15 +23,59 @@ def benchmark_graph_loading(graph_type, graph_name):
     g.parse("https://brickschema.org/schema/1.4/Brick.ttl", format="turtle")
     elapsed_time = time.time() - start_time
 
-    print(f"Time taken to load Brick ontology into {graph_name}: {elapsed_time:.2f} seconds")
-    print(f"Number of triples in {graph_name}: {len(g)}")
+    print(
+        f"[{graph_name}] Time taken to load Brick ontology into {graph_name}: {elapsed_time:.2f} seconds"
+    )
+    print(f"[{graph_name}] Number of triples in {graph_name}: {len(g)}")
+    return g
+
+
+def benchmark_graph_query(graph, query, graph_name):
+    """
+    Benchmark the execution of a SPARQL query on a graph.
+
+    Args:
+        graph (Graph): The graph to query.
+        query (str): The SPARQL query string.
+    """
+    start_time = time.time()
+    results = graph.query(query)
+    elapsed_time = time.time() - start_time
+
+    print(f"[{graph_name}] taken to execute query: {elapsed_time:.2f} seconds")
+    print(f"[{graph_name}] Number of results: {len(results)}")
+    return results
+
 
 def main():
     # Benchmark the default RDFlib Graph.
-    benchmark_graph_loading(Graph, "RDFlib Graph")
+    g1 = benchmark_graph_loading(Graph, "RDFlib Graph")
 
     # Benchmark the SparseMatrixGraph.
-    benchmark_graph_loading(lambda: Graph(store="sparse_matrix"), "SparseMatrixGraph")
+    g2 = benchmark_graph_loading(
+        lambda: Graph(store="sparse_matrix"), "SparseMatrixGraph"
+    )
+
+    # Example SPARQL query to benchmark.
+    query = """
+    SELECT ?path WHERE {
+            {
+            ?shape sh:property ?prop .
+            ?prop sh:path ?path .
+            }
+            UNION
+            {
+            ?path a owl:ObjectProperty .
+            }
+             FILTER (!isBlank(?path))
+    }"""
+    # Benchmark the query on the RDFlib Graph.
+    r1 = benchmark_graph_query(g1, query, "RDFlib Graph")
+    print(f"# results RDFlib Graph: {len(r1)}")
+    # Benchmark the query on the SparseMatrixGraph.
+    r2 = benchmark_graph_query(g2, query, "SparseMatrixGraph")
+    print(f"# results SparseMatrixGraph: {len(r2)}")
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from sparse-matrix-rdflib!")
-
-
-if __name__ == "__main__":
-    main()
